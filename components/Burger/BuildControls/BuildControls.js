70f806f7c13b39b72148b0af77551124ba858805
Yash Bansal <agarwaly482@gmail.com>
2020-11-13 01:40:15 +0530

    passes required disable data

--- components/Burger/BuildControls/BuildControls.js
+++ components/Burger/BuildControls/BuildControls.js
@@ -6,13 +6,14 @@ import classes from './BuildControls.css'
 const buildControls = (props) => {
     const controls = Object.keys(props.controls)
         .map( cntrl =>{
-            return <BuildControl key = {cntrl} label = {cntrl} add = {() => props.add(cntrl)} remove = {() => props.remove(cntrl)}/>
+            return <BuildControl key = {cntrl} disable = {props.disable[cntrl]} label = {cntrl} add = {() => props.add(cntrl)} remove = {() => props.remove(cntrl)}/>
         })
 
     return (
         <div className = {classes.BuildControls}>
             <p><strong>Total Price : </strong> {props.totalPrice}</p>
             {controls}
+            <button className = {classes.OrderButton} disabled = {!props.purchasable}>Order Now</button>
         </div>
     )
 }
