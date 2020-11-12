8c71be956a3f43d3bde6f7f42d945ba80d9a2268
Yash Bansal <agarwaly482@gmail.com>
2020-11-13 01:39:22 +0530

    handled less button and order now button

--- components/Burger/BuildControls/BuildControl/BuildControl.js
+++ components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -6,7 +6,7 @@ const buildControl = (props) => {
     return (
     <div className = {classes.BuildControl}>
         <div className = {classes.Label}>{props.label}</div>
-        <button className = {classes.Less} onClick = {props.remove}>Less</button>
+        <button className = {classes.Less} onClick = {props.remove} disabled = {props.disable}>Less</button>
         <button className = {classes.More} onClick = {props.add}>More</button>
     </div>
 
