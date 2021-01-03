216795f7c11b441fc7fd3a03f670a41c20fa171e
Yash Bansal <agarwaly482@gmail.com>
2021-01-04 00:08:47 +0530

    Navigation components

---
+++ components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import classes from './Toolbar.css';
+
+function Toolbar() {
+    return (
+        <header className = {classes.Toolbar}>
+            <div>MENU</div>
+            
+        </header>
+    )
+}
+
+export default Toolbar;
